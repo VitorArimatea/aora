618fe8e4e13d156bec34d4e2ae47097c082fe97c
Vitor Arimatea <vitorodev@outlook.com>
2024-06-29 11:16:56 -0300

    new: Auth System created

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,86 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+import { images } from '../../constants'
+import FormField from '../../components/FormField'
+import CustomButton from '../../components/CustomButton'
+import { Link, router } from 'expo-router'
+
+import { signIn } from '../../lib/appwrite'
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  })
+
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const submit = async () => {
+    if (form.email === "" || form.password === "") {
+      Alert.alert("Error", 'Please fill in all fields');
+      return;
+    }
+
+    setIsSubmitting(true);
+    try {
+      await signIn(form.email, form.password);
+      
+      // Set it to global state
+      // Example: globalState.setUser(result);
+
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  }
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full min-h-[85vh] justify-center px-4 my-6">
+          
+          <Image
+            source={images.logo}
+            className="h-[35px] w-[115px]"
+            resizeMode='contain'
+          />
+
+          <Text className="text-white text-2xl mt-10 font-psemibold">Log in to Aora</Text>
+
+          <FormField 
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({...form, email: e})}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          
+          <FormField 
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({...form, password: e})}
+            otherStyles="mt-7"
+          />
+
+          <CustomButton
+            title="Log In"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+
+          <Text className="text-[#cddce0] mt-7 text-center text-lg">Don't have an account?<Link 
+            className="text-secondary"
+            href="/sign-up"
+          > Sign Up</Link></Text>
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,11 +1,94 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+import { images } from '../../constants'
+import CustomButton from '../../components/CustomButton'
+import FormField from '../../components/FormField'
+import { Link, router } from 'expo-router'
+import { createUser } from '../../lib/appwrite'
+
 
 const SignUp = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const submit = async () => {
+    if (form.username === "" || form.email === "" || form.password === "") {
+      Alert.alert("Error", 'Please fill in all fields');
+      return;
+    }
+
+    setIsSubmitting(true);
+    try {
+      const result = await createUser(form.email, form.password, form.username);
+      
+      // Set it to global state
+      // Example: globalState.setUser(result);
+
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  }
+
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full min-h-[85vh] justify-center px-4 my-6">
+          
+          <Image
+            source={images.logo}
+            className="h-[35px] w-[115px]"
+            resizeMode='contain'
+          />
+
+          <Text className="text-white text-2xl mt-10 font-psemibold">Sign up to Aora</Text>
+
+          <FormField 
+            title="Username"
+            value={form.username}
+            handleChangeText={(e) => setForm({...form, username: e})}
+            otherStyles="mt-7"
+          />
+
+          <FormField 
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({...form, email: e})}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          
+          <FormField 
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({...form, password: e})}
+            otherStyles="mt-7"
+          />
+
+          <CustomButton
+            title="Sign Up"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+
+          <Text className="text-[#cddce0] mt-7 text-center text-lg">Already have an account?<Link 
+            className="text-secondary"
+            href="/sign-in" 
+          > Login</Link></Text>
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
